ca0c134913784dfb41fe6c24af3c72b4cdcb43cd
jcrivenaes <jcrivenaes@users.noreply.github.com>
2021-02-22 14:56:07 +0000

    Deploying to gh-pages from @ jcrivenaes/bomfasttools@f437cfeda268ef9ef64944653e00463c7f223533 🚀

--- example.js
+++ example.js
@@ -7,7 +7,7 @@ new Chart(document.getElementById("bar-chart"), {
         {
           label: "Population (millions)",
           backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-          data: [2478,5267,734,784,433]
+          data: [2478,5267,734,784,4303]
         }
       ]
     },
